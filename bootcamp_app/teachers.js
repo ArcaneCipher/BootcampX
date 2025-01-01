04d4609e5dd453712f41f386abadf0ce1c4c644e
ArcaneCipher <desjardins.jarp@gmail.com>
2024-12-31 23:07:07 -0500

    Add students.js and teachers.js implementation

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,23 +7,24 @@ const pool = new Pool({
   database: "bootcampx",
 });
 
-const cohortName = process.argv[2];
-const limit = process.argv[3] || 5;
+const cohortName = process.argv[2] || 'JUL02';
 
 const query = `
-SELECT students.id as student_id, students.name as name, cohorts.name as cohort
-FROM students
-JOIN cohorts ON cohorts.id = cohort_id
+SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
+FROM teachers
+JOIN assistance_requests ON teacher_id = teachers.id
+JOIN students ON student_id = students.id
+JOIN cohorts ON cohort_id = cohorts.id
 WHERE cohorts.name LIKE $1
-LIMIT $2;
+ORDER BY teacher;
 `;
 
 pool
-  .query(query, [`%${cohortName}%`, limit])
+  .query(query, [`%${cohortName}%`])
   .then((res) => {
     res.rows.forEach((user) => {
       console.log(
-        `${user.name} has an id of ${user.student_id} and was in the ${user.cohort} cohort`
+        `${user.cohort}: ${user.teacher}`
       );
     });
   })
